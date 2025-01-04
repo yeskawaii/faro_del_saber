09b5fb805d0f36d23a624dc6da0e73a16e821130
yeskawaii <yescasjulio7@gmail.com>
2025-01-03 23:34:51 -0600

    animation on header with logos and loading page added

--- scrypts.js
+++ scrypts.js
@@ -8,3 +8,19 @@
         }, 300); // 300ms igual que la duración de la animación
     });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const titleSpans = document.querySelectorAll('.top-bar .title span');
+    
+    // Cuando el contenedor es 'hovered', aplicamos el retraso secuencial a cada letra
+    document.querySelector('.top-bar .title').addEventListener('mouseenter', () => {
+        titleSpans.forEach((span, index) => {
+            // Ajustamos el retraso de cada letra para que la animación de la letra anterior termine antes de que comience la siguiente
+            span.style.animationDelay = `${index * 0.2}s`; // Retraso secuencial
+        });
+    });
+});
+
+window.addEventListener('load', function() {
+    document.querySelector('.loader-container').style.display = 'none';
+});
